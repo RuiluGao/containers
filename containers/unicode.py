038b1eb38d5b68dee0aba0923e22d493bf59b4d1
Ruilu Gao <Ruilu.Gao.23@cmc.edu>
2021-04-06 19:28:00 -0700

    Unicode first try

--- containers/unicode.py
+++ containers/unicode.py
@@ -20,25 +20,29 @@ class NormalizedStr:
     '''
 
     def __init__(self, text, normal_form='NFC'):
-        pass
+        self.nf = normal_form
+        self.text = unicodedata.normalize(self.nf, text)
 
     def __repr__(self):
         '''
         The string returned by the __repr__ function should be valid python code
         that can be substituted directly into the python interpreter to reproduce an equivalent object.
         '''
+        return 'NormalizedStr(\'' + self.text + '\', \'' + self.nf + '\')'
 
     def __str__(self):
         '''
         This functions converts the NormalizedStr into a regular string object.
         The output is similar, but not exactly the same, as the __repr__ function.
         '''
+        return self.text
 
     def __len__(self):
         '''
         Returns the length of the string.
         The expression `len(a)` desugars to a.__len__().
         '''
+        return len(self.__str__())
 
     def __contains__(self, substr):
         '''
@@ -48,22 +52,27 @@ class NormalizedStr:
         HINT:
         You should normalize the `substr` variable to ensure that the comparison is done semantically and not syntactically.
         '''
+        sub_norm = unicodedata.normalize(self.nf, substr)
+        return sub_norm in self.text
 
     def __getitem__(self, index):
         '''
         Returns the character at position `index`.
         The expression `a[b]` desugars to `a.__getitem__(b)`.
         '''
+        return self.text[index]
 
     def lower(self):
         '''
         Returns a copy in the same normalized form, but lower case.
         '''
+        return self.text.lower()
 
     def upper(self):
         '''
         Returns a copy in the same normalized form, but upper case.
         '''
+        return self.text.upper()
 
     def __add__(self, b):
         '''
@@ -74,6 +83,9 @@ class NormalizedStr:
         The addition of two normalized strings is not guaranteed to stay normalized.
         Therefore, you must renormalize the strings after adding them together.
         '''
+        Sa = unicodedata.normalize(self.nf, Sa.__str__())
+        Sb = unicodedata.normalize(self.nf, Sb.__str__())
+        return Sa + Sb
 
     def __iter__(self):
         '''
@@ -82,3 +94,17 @@ class NormalizedStr:
         You'll need to define your own iterator class with the appropriate magic methods,
         and return an instance of that class here.
         '''
+        return NormStrIter(self.__str__())
+
+
+class NormStrIter:
+    def __init__(self, text):
+        self.text = text
+        self.i = 0
+
+    def __next__(self):
+        if self.i == len(self.text) - 1 :
+            raise StopIteration
+        else:
+            return self.text[self.i]
+            self.i += 1
